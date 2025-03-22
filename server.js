e3fc058e2db02121757785ecc5dc05612448d71d
Priyanka Mehta <primehta17@gmail.com>
2025-03-22 20:59:09 +0530

    origin allowed

--- server.js
+++ server.js
@@ -16,11 +16,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use(express.json());
+const allowedOrigins = ['http://localhost:3000', 'https://wedigitproject-frontend.vercel.app'];
+
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
+// app.use(function(req, res, next) {
+//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); 
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   next();
+// });
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/claims', claimRoutes);
 
